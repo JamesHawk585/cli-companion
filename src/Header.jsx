e105d63fae573ada4219f1f26c1277b82ff6baff
James Hawk <jph94880@gmail.com>
2024-03-01 20:25:59 -0600

    Adds Header.jsx

---
+++ src/Header.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <main>
+      <header>
+        <h1>💻CLI-Companion</h1>
+        <div>
+          <label>
+            🔎 <input type="search"></input>
+          </label>
+        </div>
+      </header>
+    </main>
+  );
+};
+
+export default Header;
