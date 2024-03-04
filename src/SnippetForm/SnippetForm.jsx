3200f5e1513ea4d9983b3671804a3fb1d1c132e4
James Hawk <jph94880@gmail.com>
2024-03-03 20:47:44 -0600

    Passes dialogRef to SnippetForm.jsx

--- src/SnippetForm/SnippetForm.jsx
+++ src/SnippetForm/SnippetForm.jsx
@@ -1,40 +1,40 @@
-import React, { dialogRef } from 'react'
-import "./SnippetForm.css"
-import "/home/jph94880/development/code/projects/cli-companion/src/App.css"
+import React, { dialogRef } from "react";
+import "./SnippetForm.css";
+import "/home/jph94880/development/code/projects/cli-companion/src/App.css";
 
-const SnippetForm = () => {
+const SnippetForm = ({ dialogRef }) => {
   return (
-    // <dialog ref={dialogRef}>
-        <form className="form">
-          <label className='title-label'>
-            Title
-            <input name="title" />
-          </label>
-          <label>
-            Tags
+    <dialog ref={dialogRef}>
+      <form className="form">
+        <label className="title-label">
+          Title
+          <input name="title" />
+        </label>
+        <label>
+          Tags
           <input className="tags"></input>
-          </label>
-          <label>
-            Language
-            <select>
-              <option>JavaScript</option>
-              <option>Python</option>
-              <option>HTML</option>
-              <option>CSS</option>
-            </select>
-          </label>
-          <label>
-            Code
-            <textarea name="code"></textarea>
-          </label>
-          <label>
-            Explanation
-            <textarea name="explanation"></textarea>
-          </label>
-          <button>Save</button>
-        </form>
-      // </dialog>
-  )
-}
+        </label>
+        <label>
+          Language
+          <select>
+            <option>JavaScript</option>
+            <option>Python</option>
+            <option>HTML</option>
+            <option>CSS</option>
+          </select>
+        </label>
+        <label>
+          Code
+          <textarea name="code"></textarea>
+        </label>
+        <label>
+          Explanation
+          <textarea name="explanation"></textarea>
+        </label>
+        <button>Save</button>
+      </form>
+    </dialog>
+  );
+};
 
-export default SnippetForm
+export default SnippetForm;
