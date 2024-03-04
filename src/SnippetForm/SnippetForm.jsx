29c7a4b149dbfa932630b1be78074d3caba06c08
James Hawk <jph94880@gmail.com>
2024-03-03 22:18:28 -0600

    Successfully posts snippet object to db.json

--- src/SnippetForm/SnippetForm.jsx
+++ src/SnippetForm/SnippetForm.jsx
@@ -1,22 +1,39 @@
-import React, { dialogRef } from "react";
+import React, { dialogRef, useEffect } from "react";
 import "./SnippetForm.css";
 import "/home/jph94880/development/code/projects/cli-companion/src/App.css";
 
+const API = 'http://127.0.0.1:5555/snippets'
+
 const SnippetForm = ({ dialogRef }) => {
+  const onSubmit = (e) => {
+    e.preventDefault();
+    console.log(e.target);
+    const formData = Object.fromEntries(new FormData(e.target));
+    console.log(formData);
+    fetch(API, {
+      method: "POST",
+      headers: {
+        "accept": "application/json",
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(formData)
+    });
+  };
+
   return (
     <dialog ref={dialogRef}>
-      <form className="form">
+      <form className="form" onSubmit={(e) => onSubmit(e)}>
         <label className="title-label">
           Title
           <input name="title" />
         </label>
-        <label>
+        <label className="tag-label">
           Tags
-          <input className="tags"></input>
+          <input className="tags" name="tags" />
         </label>
         <label>
           Language
-          <select>
+          <select name="language-select">
             <option>JavaScript</option>
             <option>Python</option>
             <option>HTML</option>
