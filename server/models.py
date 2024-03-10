865b6b4b67f25d5bf3f4ffed830c38980102a0b0
James Hawk <jph94880@gmail.com>
2024-03-10 11:26:21 -0500

    Adds boilerplate for user model IAM in models.py

--- server/models.py
+++ server/models.py
@@ -4,6 +4,12 @@ from sqlalchemy_serializer import SerializerMixin
 from config import db, bcrypt
 
 
+
+# 1. [] db relationships 
+# 2. [] Add contraints
+# 3. [] Add valdiations 
+# 4. [x] IAM for user model 
+
 class user(db.Model):
     user_id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.Integer, primary_key=True)
@@ -11,6 +17,21 @@ class user(db.Model):
     last_name = db.Column(db.string)
     username = db.Column(db.string, unique=True)
     _password_hash = db.Column(db.string)
+
+    @hybrid_property # Restrict access to the password hash.
+    def password_hash(self):
+        raise Exception("Password hashes may not be viewed.")
+
+    @password_hash.setter # Generate a Bcrypt password hash and set it to the _password_hash attribute
+    def password_hash(self, password):
+        bcrypt_hash = bcrypt.generate_password_hash(password).decode("utf-8")
+        self._password_hash = bcrypt_hash
+
+    def authenticate(self, password): # Check if the provided password matches the one stored in the db
+        return bcrypt.check_password_hash(self._password_hash, password)
+
+    def __repr__(self):
+        return f"User {self.username}, ID: {self.id}"
 
 
 class snippet(db.Model, SerializerMixin):
