bc7abaabf32aa293eaab0d07fc2b36d402a392ee
James Hawk <jph94880@gmail.com>
2024-03-12 20:19:24 -0500

    Adds route testing notes to routes-testing-notes.md in server directory

--- server/migrations/versions/9a6c21b6db40_create_tables.py
+++ server/migrations/versions/9a6c21b6db40_create_tables.py
@@ -1,8 +1,8 @@
 """Create tables
 
-Revision ID: 9eaf4f4c650c
+Revision ID: 9a6c21b6db40
 Revises: 
-Create Date: 2024-03-12 19:23:36.041494
+Create Date: 2024-03-12 19:43:30.858390
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '9eaf4f4c650c'
+revision = '9a6c21b6db40'
 down_revision = None
 branch_labels = None
 depends_on = None
@@ -26,10 +26,10 @@ def upgrade():
     op.create_table('user',
     sa.Column('id', sa.Integer(), nullable=False),
     sa.Column('username', sa.String(length=100), nullable=False),
-    sa.Column('email', sa.String(), nullable=False),
+    sa.Column('email', sa.String(), nullable=True),
     sa.Column('first_name', sa.String(length=100), nullable=False),
     sa.Column('last_name', sa.String(length=100), nullable=False),
-    sa.PrimaryKeyConstraint('id', 'email'),
+    sa.PrimaryKeyConstraint('id'),
     sa.UniqueConstraint('username')
     )
     op.create_table('snippet',
---
+++ server/routes-testing-notes.md
@@ -0,0 +1,38 @@
+# Routes 
+
+
+## Users
+
+1. http://127.0.0.1:5000/users
+    - [x] GET
+    - [x] POST
+
+1. http://127.0.0.1:5000/users/id
+    - [x] GET
+    - [] PATCH
+    - [] DELETE
+
+## Snippets
+
+1. http://127.0.0.1:5000/snippets
+    - [x] GET
+    - [] POST
+
+
+Can't test snippets/id until snippets table is seeded. 
+1. http://127.0.0.1:5000/snippets/id
+    - [] GET
+    - [] PATCH
+    - [] DELETE
+
+## Tags 
+
+1. http://127.0.0.1:5000/tags
+    - [x] GET
+    - [x] POST
+
+1. http://127.0.0.1:5000/tags/id
+    - [x] GET
+    - [x] PATCH
+    - [x] DELETE
+
